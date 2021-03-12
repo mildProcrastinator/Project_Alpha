56b7a63189ab9a0885d124fbaa26a741cd51d72a
Zechospartan <zec12daok@hotmail.com>
2021-03-12 21:25:22 +1100

    added gun aiming

---
+++ Assets/Aim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    float mouseX, mouseY;
+    Transform Gun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Gun = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+        Gun.rotation = Quaternion.Euler(mouseX, mouseY, 0);
+
+    }
+}
