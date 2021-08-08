4ce5ff5aeadd04cec4031ac4f83c4885a872db57
Mohamad Fozan <mfozan2@gmail.com>
2021-08-08 14:09:48 +1000

    added first iteration of puzzle

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class CollisionDetection : MonoBehaviour
 {
+    public int amount;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(collision.gameObject.tag.ToString());
         if (collision.gameObject.tag.Equals("Player"))
         {
-            collision.gameObject.GetComponent<Gun>().ammo += 5;
+            collision.gameObject.GetComponent<Gun>().ammo += amount;
             Destroy(gameObject);
         }
     }
